8d72b886f85a4814a921775e27422de7ce30cf1c
zhonghaiyang <3207025105@qq.com>
2025-03-07 18:02:30 +0800

    feat: 保存版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/monorepo-template/'
+  base: '/monorepo-template/', // 设置公共路径
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: false, // 运行自动打开浏览器
+    // 开发环境中代理配置
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true, // 生产环境是否生成 source map 源码映射文件
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue'] // 将 vue 单独打包成一个 chunk
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
